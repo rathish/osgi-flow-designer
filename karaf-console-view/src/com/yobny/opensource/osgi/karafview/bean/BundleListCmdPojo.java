f1548742b57e0fe97926c2b0e9086aa6bb4f6bc8
rathishraj <rathishraj@4903a484-c4d9-a7ea-c56e-b06221144836>
2010-09-21 18:47:13 +0000

    Bundle Repository Preference Implemented

---
+++ karaf-console-view/src/com/yobny/opensource/osgi/karafview/bean/BundleListCmdPojo.java
@@ -0,0 +1,26 @@
+package com.yobny.opensource.osgi.karafview.bean;
+
+public class BundleListCmdPojo {
+
+	private String presentationName;
+	
+	public String getPresentationName() {
+		return presentationName;
+	}
+
+	public void setPresentationName(String presentationName) {
+		this.presentationName = presentationName;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	private String version;
+	
+	
+}
